5dc14f77ad726d899b0fe026f835f9458b647b50
qolkrex <qolkrex@gmail.com>
2025-08-02 13:56:57 -0500

    actualizando partner bodega bitcoin

--- src/components/about/AboutPartners/AboutPartners.jsx
+++ src/components/about/AboutPartners/AboutPartners.jsx
@@ -1,62 +1,73 @@
-import Image from 'next/image'
-import React from 'react'
-import classes from './about-partners.module.scss'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import classes from "./about-partners.module.scss";
 
 export const AboutPartners = () => {
-    return (
-        <div
-            className={classes.about_partners}
-        >
-            <div className={classes.frametop}></div>
-            <h2>Asociados</h2>
-            <div className={classes.about_partners__slider}>
-                <div>
-                    <div>
-                        <div>
-                            <Image
-                                src="/img/partnerships/image_01.png"
-
-                                width={100}
-                                height={100}
-                                alt="image"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <Image
-                                src="/img/partnerships/image_02.png"
-
-                                width={100}
-                                height={100}
-                                alt="image"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <Image
-                                src="/img/partnerships/image_03.png"
-
-                                width={100}
-                                height={100}
-                                alt="image"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <Image
-                                src="/img/partnerships/image_04.png"
-
-                                width={100}
-                                height={100}
-                                alt="image"
-                            />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className={classes.about_partners}>
+      <div className={classes.frametop}></div>
+      <h2>Asociados</h2>
+      <div className={classes.about_partners__slider}>
+        <div>
+          <div>
+            <div>
+              <Image
+                src="/img/partnerships/image_01.png"
+                width={100}
+                height={100}
+                alt="image"
+              />
             </div>
+          </div>
+          <div>
+            <div>
+              <Image
+                src="/img/partnerships/image_02.png"
+                width={100}
+                height={100}
+                alt="image"
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Image
+                src="/img/partnerships/image_03.png"
+                width={100}
+                height={100}
+                alt="image"
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Image
+                src="/img/partnerships/image_04.png"
+                width={100}
+                height={100}
+                alt="image"
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Link
+                href="https://bodegabitcoin.net"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/img/partnerships/image_05.png"
+                  width={100}
+                  height={100}
+                  alt="image"
+                />
+              </Link>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
